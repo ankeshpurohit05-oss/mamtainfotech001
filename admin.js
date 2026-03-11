28d3780c9af917afd22c3173f5a012e406f22b98
ankesh purohit <ankeshpurohit05@gmail.com>
2026-03-11 20:40:19 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,19 @@
+fetch("/api/orders")
+.then(res=>res.json())
+.then(data=>{
 
+let revenue=0
+
+data.forEach(o=>{
+revenue+=o.total
+})
+
+document.getElementById("stats").innerHTML=`
+
+Total Orders: ${data.length}<br>
+Revenue: ₹${revenue}<br>
+Profit: ₹${revenue*0.3}
+
+`
+
+})
