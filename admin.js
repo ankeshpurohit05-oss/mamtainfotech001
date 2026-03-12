d579c23ca2032a256e14735ecee306d60bc3b2b9
ankesh purohit <ankeshpurohit05@gmail.com>
2026-03-12 23:19:10 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,72 +1,39 @@
-async function loadAdmin(){
+function loadAdmin(){
 
-const orderRes = await fetch("/api/orders")
+let orders = JSON.parse(localStorage.getItem("orders")) || []
 
-const orders = await orderRes.json()
+let revenue = 0
+let cost = 0
+let count = 0
 
-const productRes = await fetch("/api/products")
-
-const products = await productRes.json()
-
-let revenue=0
-
-let cost=0
-
-let orderCount=0
-
-const today=new Date()
+const today = new Date()
 
 orders.forEach(order=>{
 
-let orderDate=new Date(order.date)
+let orderDate = new Date(order.date)
 
-let days=(today-orderDate)/(1000*60*60*24)
+let days = (today - orderDate) / (1000*60*60*24)
 
-if(days<=30){
+if(days <= 30){
 
-orderCount++
+count++
 
-revenue+=order.total
+revenue += order.total
 
 order.items.forEach(item=>{
-
-let p=products.find(prod=>prod.name===item.name)
-
-if(p){
-
-cost+=p.price*0.6
-
-}
-
+cost += item.price * 0.6
 })
 
 }
 
 })
 
-let profit=revenue-cost
+let profit = revenue - cost
 
-document.getElementById("orders").innerText=orderCount
-document.getElementById("revenue").innerText=revenue
-document.getElementById("cost").innerText=Math.round(cost)
-document.getElementById("profit").innerText=Math.round(profit)
-
-const stockDiv=document.getElementById("stock")
-
-products.forEach(p=>{
-
-stockDiv.innerHTML+=`
-
-<div>
-
-${p.name} | Price ₹${p.price} | Stock ${p.stock}
-
-</div>
-<hr>
-
-`
-
-})
+document.getElementById("orders").innerText = count
+document.getElementById("revenue").innerText = revenue
+document.getElementById("cost").innerText = Math.round(cost)
+document.getElementById("profit").innerText = Math.round(profit)
 
 }
 
