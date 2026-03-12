908aed0955045abd76578e7f53e339a6478f110c
ankesh purohit <ankeshpurohit05@gmail.com>
2026-03-12 23:04:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,113 @@
+let cart=[]
+let total=0
+
+async function loadProducts(){
+
+const res = await fetch("/api/products")
+
+const products = await res.json()
+
+const grid = document.querySelector(".product-grid")
+
+grid.innerHTML=""
+
+products.forEach(p=>{
+
+const div=document.createElement("div")
+
+div.className="product"
+
+div.innerHTML=`
+
+<img src="${p.image}" width="200">
+
+<h3>${p.name}</h3>
+
+<p>₹${p.price}</p>
+
+<p>Stock: ${p.stock}</p>
+
+<button onclick="addToCart('${p.name}',${p.price})">
+Add To Cart
+</button>
+
+`
+
+grid.appendChild(div)
+
+})
+
+}
+
+function addToCart(name,price){
+
+cart.push({name,price})
+
+total+=price
+
+updateCart()
+
+}
+
+function updateCart(){
+
+const list=document.getElementById("cart-items")
+
+const totalBox=document.getElementById("total")
+
+list.innerHTML=""
+
+cart.forEach(item=>{
+
+const li=document.createElement("li")
+
+li.innerText=item.name+" ₹"+item.price
+
+list.appendChild(li)
+
+})
+
+totalBox.innerText=total
+
+}
+
 async function placeOrder(){
 
 if(cart.length===0){
+
 alert("Cart empty")
+
 return
+
 }
 
-let order = {
-items: cart,
-total: total
+let order={
+
+items:cart,
+total:total
+
 }
 
 await fetch("/api/orders",{
+
 method:"POST",
+
 headers:{
+
 "Content-Type":"application/json"
+
 },
+
 body:JSON.stringify(order)
+
 })
 
 let message="Order from Mamta Infotech:%0A"
 
 cart.forEach(item=>{
+
 message+=item.name+" ₹"+item.price+"%0A"
+
 })
 
 message+="Total ₹"+total
@@ -33,6 +119,8 @@ total=0
 
 updateCart()
 
-alert("Order placed successfully")
+alert("Order placed")
 
 }
+
+window.onload=loadProducts
