a012488d7566cb06e18250069f968cbdbb327bd3
ankesh purohit <ankeshpurohit05@gmail.com>
2026-03-12 21:34:54 +0530

    Update products.js

--- api/products.js
+++ api/products.js
@@ -1,13 +1,56 @@
-export default function handler(req,res){
+export default function handler(req, res) {
 
 res.status(200).json([
-
-{name:"HP 13AY Display",price:9999,stock:5,image:"1.jpeg",category:"display"},
-{name:"HP 14DH Display",price:5999,stock:7,image:"2.jpeg",category:"display"},
-{name:"M120 Wireless Mouse",price:415,stock:10,image:"3.jpeg",category:"mouse"},
-{name:"M10 Wired Mouse",price:185,stock:15,image:"4.jpeg",category:"mouse"},
-{name:"Acer Charger",price:950,stock:8,image:"7.jpeg",category:"charger"}
-
+{
+name:"HP 13AY Display",
+price:9999,
+image:"1.jpeg"
+},
+{
+name:"HP 14DH Display",
+price:5999,
+image:"2.jpeg"
+},
+{
+name:"M120 Wireless Mouse",
+price:415,
+image:"3.jpeg"
+},
+{
+name:"M10 Wired Mouse",
+price:185,
+image:"4.jpeg"
+},
+{
+name:"Acer PH315-51 Touchpad",
+price:3500,
+image:"5.jpeg"
+},
+{
+name:"HP 15FA Blue Touchpad",
+price:5000,
+image:"6.jpeg"
+},
+{
+name:"Acer 135W Charger",
+price:950,
+image:"7.jpeg"
+},
+{
+name:"HP Laptop Bag",
+price:700,
+image:"8.jpeg"
+},
+{
+name:"Consistent Monitor",
+price:1850,
+image:"9.jpeg"
+},
+{
+name:"Dell Original Adapter",
+price:1000,
+image:"10.jpeg"
+}
 ])
 
 }
