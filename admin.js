6cbb338659b6ad04367a0ffe584d216b8485f3bb
ankesh purohit <ankeshpurohit05@gmail.com>
2026-03-12 22:10:46 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -4,23 +4,30 @@ const res = await fetch("/api/products")
 
 const products = await res.json()
 
-const stockDiv=document.getElementById("stock")
+const stockDiv = document.getElementById("stock")
+
+let revenue = 0
+
+let cost = 0
 
 products.forEach(p=>{
 
-stockDiv.innerHTML+=`
-
+stockDiv.innerHTML += `
 <div>
-
 ${p.name} | Price ₹${p.price} | Stock ${p.stock}
-
 </div>
-
 <hr>
-
 `
 
+revenue += p.price * (10 - p.stock)
+
+cost += p.price * 0.6
+
 })
+
+document.getElementById("revenue").innerText = revenue
+
+document.getElementById("profit").innerText = revenue - cost
 
 }
 
